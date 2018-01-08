211c1b1320fd2d789268cea72792ac22167af551
Kavie <klavieblog@gmail.com>
2018-01-08 17:11:20 +0800

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -185,7 +185,7 @@ USE_TZ = True
 # css is added, may add js, img for future purposes
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
