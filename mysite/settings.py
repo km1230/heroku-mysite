73bf457330faa7449bd8406083aaa52cae9a7be7
Kavie <moklavie@gmail.com>
2018-02-16 18:31:37 +1100

    55 AWS w/o whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -198,9 +198,9 @@ USE_TZ = True
 
 
 #Whitenoise
+"""
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-"""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
